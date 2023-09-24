ad330a16ee4f6a1ee2f195cb296612ecf41edfda
raquel <raqueel_@live.com>
2023-09-24 17:17:26 -0300

    Ajuste do sgdb pro postgres

--- organiza/src/main/java/com/example/organiza/model/Usuario.java
+++ organiza/src/main/java/com/example/organiza/model/Usuario.java
@@ -5,9 +5,11 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.Data;
 @Data 
 @Entity
+@Table (name="usuario")
 public class Usuario {
 	
 	@Id
